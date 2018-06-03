88e36cb624b03489b449542c925509471437344f
god-lfa <god-lfa@users.noreply.github.com>
2018-06-04 00:21:12 +0800

    Initial commit

---
+++ src/main/java/com/kingtop/springboot/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.kingtop.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/helloworld")
+    public String helloworld() {
+        return "helloworld333";
+    }
+    
+    @GetMapping("/helloworld2")
+    public String helloworld2() {
+        return "helloworld2";
+    }
+}
